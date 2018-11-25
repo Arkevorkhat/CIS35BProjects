0313b76b07849dcae075a1951911f9386501a2bd
Arkevorkhat <Arkevorkhat@outlook.com>
2018-11-24 20:38:45 -0800

    Added a few lines, moving to desktop.

--- AutoProjectClient/src/remote/AutoRequest.java
+++ AutoProjectClient/src/remote/AutoRequest.java
@@ -3,12 +3,16 @@ package remote;
 import java.io.NotSerializableException;
 import java.io.Serializable;
 
-public class AutoRequest implements Serializable{
+public class AutoRequest<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
 	public AutoHeader head;
 	public Object body;
-	public AutoRequest(AutoHeader head, Object body) throws NotSerializableException{
+	public AutoRequest(AutoHeader head, T body) throws NotSerializableException {
 		this.head = head;
-		if(bod)
-		this.body = body;
+		if (body instanceof Serializable) {
+			this.body = body;
+		} else {
+			throw new NotSerializableException();
+		}
 	}
 }
